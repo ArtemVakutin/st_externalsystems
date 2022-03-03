01ef285311ee80428013235bc5f27357113164aa
ArtemVakutin <artv@bk.ru>
2022-03-03 16:39:58 +0600

    lesson 91

--- network/src/main/java/ru/bk/artv/network/Client.java
+++ network/src/main/java/ru/bk/artv/network/Client.java
@@ -5,25 +5,49 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1",1000);
-
-        InputStream inputStream = socket.getInputStream();
-        OutputStream outputStream = socket.getOutputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream));
-        String command = "Artv";
-        bw.write(command);
-        bw.newLine();
-        bw.flush();
-        System.out.println("Передаем строку");
-        String s = br.readLine();
-        System.out.println(s);
-
-        inputStream.close();
-        outputStream.close();
-
-        socket.close();
+        for (int i = 0; i < 12; i++) {
+            SimpleClient simpleClient = new SimpleClient(i);
+            simpleClient.start();
+        }
 
     }
 
+}
+
+class SimpleClient extends Thread {
+
+    int cmdNumber;
+    public static final String[] COMMAND = new String[]{
+            "HELLO", "MORNING", "DAY", "EVENING"
+    };
+
+    public SimpleClient(int cmdNumber){
+        this.cmdNumber = cmdNumber;
+
+    }
+
+    @Override
+    public void run(){
+        try {
+            Socket socket = new Socket("127.0.0.1",1000);
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream));
+            String command = COMMAND[cmdNumber % COMMAND.length];
+            String sb = command + " Artv";
+            bw.write(sb);
+            bw.newLine();
+            bw.flush();
+            System.out.println("Передаем строку");
+
+            String s = br.readLine();
+            System.out.println(s);
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+    }
 }
--- network/src/main/java/ru/bk/artv/network/Server.java
+++ network/src/main/java/ru/bk/artv/network/Server.java
@@ -7,32 +7,69 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(1000);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket serverSocket = new ServerSocket(1000, 1000); //second is limit of request
         while (true){
             Socket socket = serverSocket.accept();
             System.out.println("Server.main коннектнулся");
-            handleRequest(socket);
+            new SimpleServer(socket).start();
+        }
+    }
+}
 
+class SimpleServer extends Thread{
+    private Socket socket;
+
+    public SimpleServer(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run(){
+        handleRequest();
+    }
+
+    private void handleRequest(){
+
+        try {
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream));
+            String request = br.readLine();
+            String[] lines = request.split("\\s+");
+            String command = lines[0];
+            String userName = lines[1];
+            System.out.println("Server got string + " + command);
+            System.out.println("Server got string + " + userName);
+
+            String response = buildResponse(command, userName);
+
+            Thread.sleep(2000);
+            bw.write(response);
+            bw.newLine();
+            bw.flush();
+            bw.close();
+            br.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
-    private static void handleRequest(Socket socket) throws IOException {
-        InputStream inputStream = socket.getInputStream();
-        OutputStream outputStream = socket.getOutputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream));
-        System.out.println("Создаем строку");
-        StringBuilder sb = new StringBuilder("Hello ");
-        String userName = br.readLine();
+    private String buildResponse(String command, String userName) {
+        if(command.equals("HELLO")){
+            return ("Hello " + userName);
+        } if (command.equals("MORNING")){
+            return ("Good morning " + userName);
+        }if (command.equals("DAY")){
+            return ("Good day " + userName);
+        } if (command.equals("EVENING")) {
+            return ("Good evening " + userName);
+        } else {
+            return "Nothing";
+        }
 
-        System.out.println("получили + " + userName);
-        sb.append(userName);
-        bw.write(sb.toString());
-        bw.newLine();
-        bw.flush();
-        bw.close();
-        br.close();
-        socket.close();
     }
 }
